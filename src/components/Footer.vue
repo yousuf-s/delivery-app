9345cd9e0ac5b971902250883cf0322c3ada00d6
yousuf-s <s.yousuf1235@gmail.com>
2025-06-21 13:10:52 +0530

    Footer changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,4 +2,15 @@
   <footer class="bg-gray-100 text-center py-4 mt-10 pb-3">
     <p class="text-sm text-gray-600">© 2025 The Protein Cart. All rights reserved.</p>
   </footer>
+  <!-- Cart block -->
+  <div
+    class="lg:hidden fixed bottom-10 z-50 flex items-center right-5 border border-gray-400 p-2.5 rounded-full shadow-md cursor-pointer justify-center md:justify-start">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+      fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+      stroke-linejoin="round" class="lucide lucide-shopping-cart mr-1">
+      <circle cx="8" cy="21" r="1"/>
+      <circle cx="19" cy="21" r="1"/>
+      <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/>
+    </svg>
+  </div>
 </template>
